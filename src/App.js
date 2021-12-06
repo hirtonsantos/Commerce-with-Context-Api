5f00cf1fa4a5b1fffbb0f3ef72cc5ea5ffea39dd
hirtonsantos <hirtonsantos.hs@gmail.com>
2021-12-05 23:06:48 -0300

    revisao de codigo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-// import Header from "./Components/Header";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Components/Pages/Routes";
-// import GlobalStyle from "./Components/styles/Global";
-// import { useCatalogue } from "./Providers/Products";
-// import ProductsList from "./Components/ProductsList"
-// import CartPage from "./Components/CartPage";
 
 function App() {
 
   return (
     <>
-      {/* <GlobalStyle /> */}
-      {/* <Header />
-      <ProductsList/> */}
-      {/* <CartPage/> */}
-      <BrowserRouter>
       <Routes/>
-      </BrowserRouter>
     </>
   );
 }
