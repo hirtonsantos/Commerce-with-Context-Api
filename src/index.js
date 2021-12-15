442eb03b8f66f3a7e593d291569424d8fa67b243
hirtonsantos <hirtonsantos.hs@gmail.com>
2021-12-14 21:29:45 -0300

    revisao de codigo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import Providers from "./Providers"
+import {Providers} from "./Providers"
 
 ReactDOM.render(
   <React.StrictMode>
